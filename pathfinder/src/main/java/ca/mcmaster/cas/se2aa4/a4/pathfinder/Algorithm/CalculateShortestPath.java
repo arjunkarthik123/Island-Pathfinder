8358de18514c3c7252d9da9a01a985a8a605e8cb
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2023-03-31 13:45:26 -0400

    set up the calculate path class

--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/Algorithm/CalculateShortestPath.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/Algorithm/CalculateShortestPath.java
@@ -1,7 +1,13 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder.Algorithm;
 
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.GraphADT.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculateShortestPath implements FindPath{
 
+    List<Node> nodeList = new ArrayList<>();
    @Override
     public void calculatePath() {
 
