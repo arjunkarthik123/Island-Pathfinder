6b8a4c1c5be045e06a88acca4ed3eb239d5cbf0b
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2023-04-03 14:34:51 -0400

    started edge class

--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/GraphADT/Edge.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/GraphADT/Edge.java
@@ -1,4 +1,13 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder.GraphADT;
 
 public class Edge {
+
+    Node n1;
+    Node n2;
+    int weight;
+    public Edge (Node node1, Node node2, int weight){
+        this.n1 = node1;
+        this.n2 = node2;
+        this.weight = weight;
+    }
 }
